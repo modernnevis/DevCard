db32d4fd1cb76ed44a5f2c12f91137730c4a8a97
yaser shakoori <yasershakoori@outlook.com>
2021-02-27 21:35:20 +0330

    index & contact page added

--- DevCard_MVC/DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/DevCard_MVC/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_MVC.Controllers
 {
@@ -13,9 +9,8 @@ namespace DevCard_MVC.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -23,7 +18,7 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
