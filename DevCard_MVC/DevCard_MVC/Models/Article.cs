0a1d15b7b826c836175f8cd29a5b7a213b05a298
yaser shakoori <yasershakoori@outlook.com>
2021-02-27 22:16:52 +0330

    project section from index page moved to partial view projects

---
+++ DevCard_MVC/DevCard_MVC/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DevCard_MVC.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+    }
+}
---
+++ DevCard_MVC/DevCard_MVC/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Client { get; set; }
+    }
+}
